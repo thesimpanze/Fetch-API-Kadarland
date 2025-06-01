214197782cb44a1f3cbad6b95eec117d638884e6
whorsyd <rasyid8nuruddin@gmail.com>
2025-06-01 22:10:21 +0700

    final commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://www.erp.kadargroup.web.id',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
